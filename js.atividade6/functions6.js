ee3840345b990a921e0f70596caa1e12035ec4a6
rafayoko <r205072@dac.unicamp.br>
2021-05-17 18:28:23 -0300

    Update functions6.js

--- js.atividade6/functions6.js
+++ js.atividade6/functions6.js
@@ -36,10 +36,10 @@ function undisplayPicture(){
 }
 
 const texto = [
-  "Essa foto foi tirada em 2018 com essa vista linda de Pebble Beach",
-  "Bateria Invocada - Minha primeira apresentação na Tabu ano de 2018",
-  "Ubatuba - 2018 - 1º Réveillon com o Rafa",
-  "Uma das minhas fotos favoritas, uso ela sempre que preciso fazer um perfiil novo. Foi tirada no Museu da Arte Sacra.",
-  "Foto tirada no Tusca de 2018. Foi o meu primeiro Tusca e neste momento tinha encontrado meus amigos depois de muito tempo perdido.",
-  "Essa foto foi tirada quando recebi minha fantasia para o desfile do carnaval de São Paulo 2019. Desfilei pela Unidos de Vila Maria na bateria, foi incrível!"
+  " Essa foto foi tirada em 2018 com essa vista linda de Pebble Beach",
+  " Bateria Invocada - Minha primeira apresentação na Tabu ano de 2018",
+  " Ubatuba - 2018 - 1º Réveillon com o Rafa",
+  " Uma das minhas fotos favoritas, uso ela sempre que preciso fazer um perfiil novo. Foi tirada no Museu da Arte Sacra.",
+  " Foto tirada no Tusca de 2018. Foi o meu primeiro Tusca e neste momento tinha encontrado meus amigos depois de muito tempo perdido.",
+  " Essa foto foi tirada quando recebi minha fantasia para o desfile do carnaval de São Paulo 2019. Desfilei pela Unidos de Vila Maria na bateria, foi incrível!"
 ]
